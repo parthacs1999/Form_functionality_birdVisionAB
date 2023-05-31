6bc7f958fbee6a50ac131553ecb82b6d17e8b923
Partha Chakraborty <partha.c1999@gmail.com>
2023-05-31 08:29:41 +0530

    Navbar Component added with styles using css

---
+++ src/components/UI/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+import { FaUserCircle } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <h2>BirdVision AB</h2>
+            <div className={styles.loginContainer}>
+                <FaUserCircle className={styles.userIcon} />
+                <p>Login</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
